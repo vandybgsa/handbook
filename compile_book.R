121b695c2cf157f1e5f9f91c158a5319c5c4566a
Lucy <ld.mcgowan@vanderbilt.edu>
2016-11-01 09:06:37 -0500

    update compile file

--- compile_book.R
+++ compile_book.R
@@ -1,7 +1,2 @@
-filename = "compile_book.R"
-filepath = file.choose()  # browse and select your_file.R in the window
-dir = substr(filepath, 1, nchar(filepath)-nchar(filename))
-setwd(dir)
-
 bookdown::render_book("index.Rmd", "bookdown::gitbook")
 
